e818a8d0b320e66a2d72f2e9679efacfb15b6b09
Abhinav Sharma <abhinav.sharma3@hotmailcom>
2025-02-25 11:21:25 -0600

    Simple interest calc

---
+++ JAVASCRIPT/lab1/script.js
@@ -0,0 +1,38 @@
+// Dynamic age calculator
+
+/*let birthyear = prompt("Enter your birth year");
+let age = calculateAge(birthyear);
+window.alert( "Your age is :"+ age);
+
+
+function calculateAge(birthyear) {
+    let currentyear = new Date().getFullYear();
+    let age = currentyear - birthyear;
+    return age;
+}*/
+
+// Simple Interest calculator
+
+let principalAmount = prompt ("Enter the principal amount");
+let rate = prompt(" Enter the rate of interest");
+let time = prompt("Enter the years");
+
+let total = simpleInterestCalculator(principalAmount, rate, time);
+window.alert("Your total simple interest amount is " + total);
+
+function simpleInterestCalculator(principalAmount, rate, time) {
+
+    if (principalAmount<=0 || rate < 0 || time <=0){
+        window.alert("Please enter the correct values");
+    }
+    else {
+        let total = (principalAmount * rate * time) / 100;
+        return total;
+    }
+}
+
+
+
+
+
+
